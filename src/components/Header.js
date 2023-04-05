96894c758d1e3f448389328ba4643e13194add54
Jayla Young <jaylayoung1@gmail.com>
2023-04-05 06:37:34 -0400

    header link updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Header(props) {
   return (
     <nav className="nav">
-      <Link to="/">
+      <Link to="/posts">
         <div>Restaurant Review App</div>
       </Link>
     </nav>
